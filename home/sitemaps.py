cb7b1204af5f2a6413269ec04e4bdec0211ec4a0
kopus <kopuskopecik@gmail.com>
2019-04-08 15:03:00 +0300

    sitemap değiştirildi

--- home/sitemaps.py
+++ home/sitemaps.py
@@ -46,7 +46,7 @@ class LessonSitemap(Sitemap):
 	protocol = "https"
 
 	def items(self):
-		return Lesson.objects.all()[0:16]
+		return Lesson.objects.all()[0:21]
 		
 	def lastmod(self, obj):
 		return obj.updating_date
