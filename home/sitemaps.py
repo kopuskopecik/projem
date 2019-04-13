bed9ca5437026fef757314e242df835f9717b17c
kopus <kopuskopecik@gmail.com>
2019-04-13 14:43:18 +0300

    46-76 arası dersler eklendi. bootstrap4 dosyaları gönderildi

--- home/sitemaps.py
+++ home/sitemaps.py
@@ -46,7 +46,7 @@ class LessonSitemap(Sitemap):
 	protocol = "https"
 
 	def items(self):
-		return Lesson.objects.all()[0:45]
+		return Lesson.objects.all()[0:76]
 		
 	def lastmod(self, obj):
 		return obj.updating_date
