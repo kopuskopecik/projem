41aa6134f827b6d8e377a78e37aef69f91bf1c7a
kopus <kopuskopecik@gmail.com>
2019-04-17 15:30:25 +0300

    example.py, sitemap, models ve views değiştirildi

--- home/sitemaps.py
+++ home/sitemaps.py
@@ -46,7 +46,7 @@ class LessonSitemap(Sitemap):
 	protocol = "https"
 
 	def items(self):
-		return Lesson.objects.all()[0:76]
+		return Lesson.objects.all()[0:100]
 		
 	def lastmod(self, obj):
 		return obj.updating_date
