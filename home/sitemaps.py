f634438d056bc16c2d5d9f1ed9db05de952d56b9
kopus <kopuskopecik@gmail.com>
2019-04-27 18:22:43 +0300

    tarih değişiklikleri yapıldı ve sitemap sayıları değiştirildi

--- home/sitemaps.py
+++ home/sitemaps.py
@@ -46,7 +46,7 @@ class LessonSitemap(Sitemap):
 	protocol = "https"
 
 	def items(self):
-		return Lesson.objects.all()[0:253]
+		return Lesson.objects.all()[0:287]
 		
 	def lastmod(self, obj):
 		return obj.updating_date
